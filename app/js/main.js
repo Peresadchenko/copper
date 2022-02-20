8a5e909ee99bba3dac9a188dbc3fe89001e8d7bb
Peresadchenko <1pers1@bk.ru>
2022-02-20 20:22:59 +0300

    add header, products, sales

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,38 @@
 $(function() {
   
+    $('.top-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        adaptiveHeight: true,
+        autoplay: false,
+        autoplaySpeed: 2000
+    });
+
+    $('.top-slider__btnPrev').click(function (event) {
+        $('.top-slider__inner').slick("slickPrev");
+    });
+    $('.top-slider__btnNext').click(function (event) {
+        $('.top-slider__inner').slick("slickNext");
+    });
+
+
+    $('.sales-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        easing: 'ease',
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        speed: 800,
+        slidesToScroll: 3,
+        autoplay: false,
+        autoplaySpeed: 2000
+    });
+
+    $('.sales-slider__btnPrev').click(function (event) {
+        $('.sales-slider__inner').slick("slickPrev");
+    });
+    $('.sales-slider__btnNext').click(function (event) {
+        $('.sales-slider__inner').slick("slickNext");
+    });
 });
