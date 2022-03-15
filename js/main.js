a568c4204d9a448913b9bd525a89d3d18525adcf
Peresadchenko <1pers1@bk.ru>
2022-03-15 17:42:30 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(function () {
 
+    //form-styler-------------------------
+
+    $('.filter-price__select').styler();
+
+    //.card-product__content-num--------------------
+
+    $('.select-style, .card-product__content-num').styler();
+
+    //slider top-------------------------
+
     $('.top-slider__inner').slick({
         arrows: false,
         dots: true,
@@ -16,6 +26,7 @@ $(function () {
         $('.top-slider__inner').slick("slickNext");
     });
 
+    //slider sales-------------------------
 
     $('.sales-slider__inner').slick({
         arrows: false,
@@ -36,6 +47,7 @@ $(function () {
         $('.sales-slider__inner').slick("slickNext");
     });
 
+    //slider novelties-------------------------
 
     $('.novelties-slider__inner').slick({
         arrows: false,
@@ -56,6 +68,7 @@ $(function () {
         $('.novelties-slider__inner').slick("slickNext");
     });
 
+    //slider recommend-------------------------
 
     $('.recommend-slider__inner').slick({
         arrows: false,
@@ -76,7 +89,7 @@ $(function () {
         $('.recommend-slider__inner').slick("slickNext");
     });
 
-
+    //slider discounts-------------------------
 
     $('.discounts-slider__inner').slick({
         arrows: false,
@@ -97,6 +110,7 @@ $(function () {
         $('.discounts-slider__inner').slick("slickNext");
     });
 
+    //slider about-------------------------
 
     $('.about-slider__inner').slick({
         arrows: false,
@@ -117,11 +131,83 @@ $(function () {
         $('.about-slider__inner').slick("slickNext");
     });
 
+    //product-slide__thumb  product-slide__big-------------------------
 
-    $(".footer a").on("click", function (e) {
+    $('.product-slide__thumb').slick({
+        asNavFor: '.product-slide__big',
+        focusOnSelect: true,
+        slidesToShow: 4.6,
+        slidesToScroll: 1,
+        arrows: false,
+        vertical: true
+
+    });
+    $('.product-slide__big').slick({
+        asNavFor: '.product-slide__thumb',
+        draggable: false,
+        arrows: false,
+        fade: true
+    });
+
+    $('.product-slide__thumb-btnPrev').click(function (event) {
+        $('.product-slide__thumb').slick("slickPrev");
+    });
+    $('.product-slide__thumb-btnNext').click(function (event) {
+        $('.product-slide__thumb').slick("slickNext");
+    });
+
+    //SCROLL--------------------
+
+    $(".footer__btn").on("click", function (e) {
         e.preventDefault();
-        var id = $(this).attr('href'),
+        var id = $(this).attr("href"),
             top = $(id).offset().top;
         $('body,html').animate({ scrollTop: top }, 1500);
     });
+
+    
+
+    //CHECKBOX--------------------
+
+    // $.each($('.checkbox'), function (index, val) {
+    //     if ($(this).find('input').prop('checked') == true) {
+    //         $(this).addClass('active');
+    //     }
+    // });
+    // $(document).on('click', 'checkbox', function (event) {
+    //     if ($(this).hasClass('active')) {
+    //         $(this).find('input').prop('checked', false);
+    //     } else {
+    //         $(this).find('input').prop('checked', true);
+    //     }
+    //     $(this).toggleClass('active');
+
+    //     return false;
+    // });
+
+    //RADIO-------------------------
+
+    $.each($('.order__form-item'), function (index, val) {
+        if ($(this).find('input').prop('checked') == true) {
+            $(this).addClass('order__form-item--active');
+        }
+    });
+    $(document).on('click', '.order__form-item', function (event) {
+        $(this).parents('.order__form-radio').find('.order__form-item').removeClass('order__form-item--active');
+        $(this).parents('.order__form-radio').find('.order__form-item input').prop('checked', false);
+        $(this).toggleClass('order__form-item--active');
+        $(this).find('input').prop('checked', true);
+        return false;
+    });
+
+    //mixitup-------------------------
+
+    var mixer = mixitup('.profile', {
+        load: {
+            filter: '.category-a'
+        }
+    });
+
+
+
 });
